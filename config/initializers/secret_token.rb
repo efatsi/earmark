dbcbcbee6a880994b4fbc43a63ba4f0845fe95d9
Eli Fatsi <eli.fatsi@viget.com>
2014-01-04 10:23:23 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Earmark::Application.config.secret_key_base = 'bf980c20d9bcf00853a05ddce75165372a743b42860883e6ea036bee23fced39a13e41c61ada5d292966cb7253ac664ac71f01d086a2bd26331ce3d0bea686a2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Earmark::Application.config.session_store :cookie_store, key: '_earmark_session'
